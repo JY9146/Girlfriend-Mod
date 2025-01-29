52687b7fd9fb83d801d9c14d36707eaae3ef1a74
Cyber Official <19499442+cyberofficial@users.noreply.github.com>
2025-01-29 03:37:39 -0500

    [update] Fix for single player and minor code change
    - Summons a villager with the GF tag.
    - Fixed for single player

--- Girlfriend-master/src/main/java/dev/vini2003/girlfriend/registry/client/GFCommands.java
+++ Girlfriend-master/src/main/java/dev/vini2003/girlfriend/registry/client/GFCommands.java
@@ -1,9 +1,12 @@
 package dev.vini2003.girlfriend.registry.client;
 
 import dev.vini2003.girlfriend.client.screen.GirlfriendScreen;
+import dev.vini2003.girlfriend.registry.common.GFNetworks;
+import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.PacketByteBuf;
 
 public class GFCommands {
 	public static void init() {
@@ -13,6 +16,13 @@ public class GFCommands {
 					MinecraftClient.getInstance().openScreen(new GirlfriendScreen());
 				});
 				
+				return 1;
+			})
+		);
+
+		ClientCommandManager.DISPATCHER.register(
+			ClientCommandManager.literal("summongf").executes(context -> {
+				ClientPlayNetworking.send(GFNetworks.SUMMON_GF, new PacketByteBuf(Unpooled.buffer()));
 				return 1;
 			})
 		);
