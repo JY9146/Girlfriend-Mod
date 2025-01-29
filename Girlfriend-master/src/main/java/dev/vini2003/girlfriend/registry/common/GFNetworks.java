55b098df492d6ea5e2c72aaff76c13f46e070510
Cyber Official <19499442+cyberofficial@users.noreply.github.com>
2025-01-29 03:37:39 -0500

    [update] Fix for single player and minor code change
    - Summons a villager with the GF tag.
    - Fixed for single player

--- Girlfriend-master/src/main/java/dev/vini2003/girlfriend/registry/common/GFNetworks.java
+++ Girlfriend-master/src/main/java/dev/vini2003/girlfriend/registry/common/GFNetworks.java
@@ -3,6 +3,9 @@ package dev.vini2003.girlfriend.registry.common;
 import dev.vini2003.girlfriend.GF;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
@@ -13,7 +16,8 @@ import net.minecraft.util.math.Vec3d;
 
 public class GFNetworks {
 	public static final Identifier FIRE = GF.id("fire");
-	
+	public static final Identifier SUMMON_GF = GF.id("summon_gf");
+
 	private static void fire(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
 		server.execute(() -> {
 			Vec3d rotation = player.getRotationVector();
@@ -25,8 +29,19 @@ public class GFNetworks {
 			player.world.spawnEntity(witherSkullEntity);
 		});
 	}
+
+	private static void summonGF(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+		server.execute(() -> {
+			VillagerEntity girlfriend = new VillagerEntity(EntityType.VILLAGER, player.world);
+			girlfriend.setCustomName(new LiteralText("Girlfriend"));
+			girlfriend.setUuid(GF.GIRLFRIEND_UUID);
+			girlfriend.refreshPositionAndAngles(player.getX(), player.getY(), player.getZ(), player.getYaw(), player.getPitch());
+			player.world.spawnEntity(girlfriend);
+		});
+	}
 	
 	public static void init() {
 		ServerPlayNetworking.registerGlobalReceiver(FIRE, GFNetworks::fire);
+		ServerPlayNetworking.registerGlobalReceiver(SUMMON_GF, GFNetworks::summonGF);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 This is the girlfriend mod from Tommyinnit (https://www.youtube.com/watch?v=TRigGOh-_ys), made by Vini2003. I can't get it to work, there seems to be errors in gradle. I'm very new to Github and don't really know any Java or Gradle. So if you know what your doing you can make a fork of it working or make a pull request or something. If anyone has found any files in some of the deleted repositories please upload them here (I'm trying to compile them all): https://www.dropbox.com/request/N19bwKETJRDn0nVWx2BO
+
+# Commands
+* Press `f` to fire tnt
+* Type `/gf` for Girlfriend overlay messages
+* Type `/summongf` to summon a villager that can be interacted with the GF overlay
+
+# Requirements to build
+- Java 16 or higher
+  - Required by: `Girlfriend-master/build.gradle`, `Girlfriend-master/gradle.properties`, `Girlfriend-master/src/main/resources/girlfriend.mixins.json`
+- Gradle 7.0 or higher
+  - Required by: `Girlfriend-master/gradle/wrapper/gradle-wrapper.properties`, `Girlfriend-master/gradlew`, `Girlfriend-master/gradlew.bat`
+- Fabric Loader 0.11.6 or higher
+  - Required by: `Girlfriend-master/gradle.properties`, `Girlfriend-master/src/main/resources/fabric.mod.json`
+- Fabric API 0.35.1+1.17 or higher
+  - Required by: `Girlfriend-master/gradle.properties`, `Girlfriend-master/src/main/resources/fabric.mod.json`
+- Minecraft 1.17
+  - Required by: `Girlfriend-master/gradle.properties`, `Girlfriend-master/src/main/resources/fabric.mod.json`
+- LibGui 4.0.0+1.17
+  - Required by: `Girlfriend-master/gradle.properties`, `Girlfriend-master/build.gradle`
